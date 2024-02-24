aab586d1560c4851e457a570d4f4335e75b1c647
marmont <mar.mont@example.com>
2024-02-24 20:08:43 +0100

    first commit

---
+++ src/main/java/PasswordHashingExample.java
@@ -0,0 +1,54 @@
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class PasswordHashingExample {
+
+    private static final Logger logger = LogManager.getLogger(PasswordHashingExample.class);
+
+    public static String generateStrongPasswordHash(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        int iterations = 1000;
+        char[] chars = password.toCharArray();
+        byte[] salt = getSalt();
+
+        PBEKeySpec spec = new PBEKeySpec(chars, salt, iterations, 64 * 8);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+
+        byte[] hash = skf.generateSecret(spec).getEncoded();
+        return iterations + ":" + toHex(salt) + ":" + toHex(hash);
+    }
+
+    private static byte[] getSalt() throws NoSuchAlgorithmException {
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        byte[] salt = new byte[16];
+        sr.nextBytes(salt);
+        return salt;
+    }
+
+    private static String toHex(byte[] array) {
+        BigInteger bi = new BigInteger(1, array);
+        String hex = bi.toString(16);
+        int paddingLength = (array.length * 2) - hex.length();
+        if(paddingLength > 0) {
+            return String.format("%0" + paddingLength + "d", 0) + hex;
+        } else {
+            return hex;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            String originalPassword = "password123";
+            String generatedSecuredPasswordHash = generateStrongPasswordHash(originalPassword);
+            System.out.println(generatedSecuredPasswordHash);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            logger.error("Failed to generate password hash", e);
+        }
+    }
+}
